11c10895280f491e298e42024a8a4c54af066487
김형래 <khr157929@gmail.com>
2021-12-06 19:21:06 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import GlobalStyles from "./components/GlobalStyles";
+import Nav from "./components/Navigation";
 
 function App() {
   return (
     <>
-      <div></div>
+      <GlobalStyles />
+      <Nav />
     </>
   );
 }
---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Navigation() {
+  return (
+    <Wrap>
+      <Header>1234</Header>
+    </Wrap>
+  );
+}
+
+const Wrap = styled.div``;
+
+const Header = styled.div``;
